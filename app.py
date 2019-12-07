95b73f923905d20d744159873577f0509329a291
McKade Sorensen <dmsorensen@alaska.edu>
2019-12-07 11:39:38 -0900

    Added comments

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from project_493 import app
 
 if __name__ == "__main__":
+    """ Run CatBook """
     app.run(debug=True, ssl_context=('cert.pem', 'key.pem'))
--- project_493/routes.py
+++ project_493/routes.py
@@ -25,16 +25,16 @@ def about() -> html:
 
 @app.route("/register", methods=['GET', 'POST'])
 def register() -> html:
-
+    """ Function for registering to CatBook """
     if current_user.is_authenticated:
          return redirect(url_for('home'))
 
     form = RegistrationForm()
     if form.validate_on_submit():
-        hashed_pswd = bcpt.generate_password_hash(form.password.data).decode('utf-8')
+        hashed_pswd = bcpt.generate_password_hash(form.password.data).decode('utf-8')          # Hash Password
         user = User(username=form.username.data, email=form.email.data, password=hashed_pswd)
-        db.session.add(user)
-        db.session.commit()
+        db.session.add(user)    # Add to the data base
+        db.session.commit()     # Commit changes to the data base
         flash(f'Account created! You can now login.', 'success')
         return redirect(url_for('login'))
     return render_template('register.html', title="Register", form=form)
@@ -42,7 +42,7 @@ def register() -> html:
 
 @app.route("/login", methods=['GET', 'POST'])
 def login() -> html:
-
+    """ Function for logging onto CatBook """
     if current_user.is_authenticated:
         return redirect(url_for('home'))
 
@@ -62,11 +62,13 @@ def login() -> html:
 
 @app.route("/logout")
 def logout() -> html:
+    """ Function for logging user out """
     logout_user()
     return redirect(url_for('home'))
 
 
 def save_picture(form_picture) -> str:
+    """ Stores photo on the server """
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_file_name = random_hex + f_ext
@@ -84,14 +86,15 @@ def save_picture(form_picture) -> str:
 @app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account() -> html:
+    """ Function for updating a users account, user must be logged in """
     form = UpdateAccountForm()
     if form.validate_on_submit():
         if form.picture.data:
-            picture_path = save_picture(form.picture.data)
-            current_user.image_file = picture_path
-        current_user.username = form.username.data
-        current_user.email = form.email.data
-        db.session.commit()
+            picture_path = save_picture(form.picture.data)  # Upload img
+            current_user.image_file = picture_path          # Set photo as Profile picture.
+        current_user.username = form.username.data          # Update password
+        current_user.email = form.email.data                # Update email
+        db.session.commit()                                 # Add new info to data base
         flash('Your CatBook has been updated!', 'success')
         return redirect(url_for('account'))
 
@@ -106,11 +109,12 @@ def account() -> html:
 @app.route("/post/new", methods=['GET', 'POST'])
 @login_required
 def new_post() -> html:
+    """ Function that allows users to make a new post, user must be logged in """
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(title=form.title.data, content=form.content.data, author=current_user)
-        db.session.add(post)
-        db.session.commit()
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)  # Get users post
+        db.session.add(post)  # Add post to data base
+        db.session.commit()   # Commit changes to the data base
         flash('Your post has been created!', 'success')
         return redirect(url_for('home'))
     return render_template('create_post.html', title='New Post', form=form, legend="New post")
@@ -125,15 +129,16 @@ def post(post_id) -> html:
 @app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
 @login_required
 def update_post(post_id) -> html:
-    post = Post.query.get_or_404(post_id)
-    if post.author != current_user:  # Something we can take away and do an attack on
-        abort(403)
+    """ Allows users to update their post, user must be logged in """
+    post = Post.query.get_or_404(post_id)  # Check to see if it is the correct user
+    if post.author != current_user:
+        abort(403)  # If not the correct user display an Error
 
     form = PostForm()
     if form.validate_on_submit():
         post.title = form.title.data
         post.content = form.content.data
-        db.session.commit()
+        db.session.commit()  # Commit changes to data base
         flash('The post has been updated!', 'success')
         return redirect(url_for('post', post_id=post.id))
 
@@ -148,11 +153,12 @@ def update_post(post_id) -> html:
 @app.route("/post/<int:post_id>/delete", methods=['POST'])
 @login_required
 def delete_post(post_id) -> html:
-    post = Post.query.get_or_404(post_id)
-    if post.author != current_user:  # Something we can take away and do an attack on
-        abort(403)
+    """ Function that allows users to delete their posts, user must be logged in """
+    post = Post.query.get_or_404(post_id)  # Check to see if it's the correct user for the post
+    if post.author != current_user:
+        abort(403)                          # If not the correct user display an Error
 
-    db.session.delete(post)
-    db.session.commit()
-    flash('Your post has been deleted!', 'success')
+    db.session.delete(post)  # Remove the post
+    db.session.commit()      # Update the database
+    flash('Your post has been deleted!', 'success')  # Notify the user their post has been deleted
     return redirect(url_for('home'))
